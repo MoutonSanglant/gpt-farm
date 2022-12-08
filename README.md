8f2ce1d084f1602d114c277bad0ab42aff28ec5a
vxdcxw <defresne.thierry@gmail.com>
2022-12-08 02:17:27 +0100

    Initial work by ChatGPT

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ferme Poule
+
+Ferme Poule is a simple program that simulates the population growth of a chicken farm over time.
+
+## Note
+
+This program has been generated using: https://chat.openai.com/chat
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+To compile and run this program, you will need a C compiler, such as GCC, installed on your system.
+
+### Installing
+
+To install the program, simply clone or download the repository and compile the code with your C compiler:
+
+$ git clone https://github.com/user/ferme-poule.git
+$ cd ferme-poule
+$ gcc ferme-poule.c -o ferme-poule
+
+
+### Running
+
+To run the program, use the following command:
+
+$ ./ferme-poule
+
+
+The program will prompt you for the initial number of chickens, the starting age of the farmer, and the ending age of the farmer. Once you have entered this information, the program will output a table showing the population of chickens for each year.
+
+## Authors
+
+* **Your Name** - *Initial work* - [FermePoule](https://github.com/user/ferme-poule)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ ferme-poule.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_NOM 10
+
+// Fonction qui vérifie que les paramètres d'entrée sont valides
+int valider_params(int argc, char* argv[], int* nb_poules, int* age_depart, int* age_fin)
+{
+  // Vérifie que les paramètres d'entrée sont valides
+  if (argc != 4)
+  {
+    printf("Usage: %s <nombre de poules> <age de depart> <age de fin>\n", argv[0]);
+    return 0;
+  }
+
+  // Récupère les paramètres d'entrée
+  *nb_poules = atoi(argv[1]);
+  *age_depart = atoi(argv[2]);
+  *age_fin = atoi(argv[3]);
+
+  // Vérifie que les paramètres d'entrée sont valides
+  if (*nb_poules <= 0 || *age_depart < 0 || *age_fin <= *age_depart)
+  {
+    printf("Les paramètres doivent être positifs et l'âge de fin doit être supérieur à l'âge de départ.\n");
+    return 0;
+  }
+
+  return 1;
+}
+
+// Fonction qui affiche l'entête du tableau
+void afficher_entete()
+{
+  // Affiche l'entête du tableau
+  printf("Annee\tAge\tPoules\tLettre\tRot\tNom\tSexe\n");
+  printf("------\t----\t------\t------\t---\t---\t----\n");
+}
+
+// Fonction qui calcule la population de poules pour chaque année
+void calculer_population(int nb_poules, int age_depart, int age_fin)
+{
+  // Variables pour la boucle
+  int annee, age, poules, rot, sexe;
+  char lettre, nom[MAX_NOM];
+
+  // Boucle sur chaque année
+  for (annee = 0; annee <= age_fin - age_depart; annee++)
+  {
+    // Calcule l'âge du fermier
+    age = age_depart + annee;
+
+    // Calcule la population de poules
+    poules = nb_poules;
+    if (annee % 2 == 0)
+      poules += poules * 0.3;
+    else
+      poules += poules * 0.4;
+    poules = (int)poules;
+
+    // Calcule le reste de la division par 26
+    rot = poules % 26;
+
+    // Détermine la lettre à utiliser pour le prénom
+    lettre = (char)(65 + rot);
+
+    // Vérifie si un enfant doit naître cette année
+    if (poules % 2 == 0)
+    {
+      // Détermine le sexe de l'enfant
+      if (annee % 2 == 0)
+        sexe = 'F';
+      else
+        sexe = 'M';
+
+      // Génère le prénom de l'enfant
+      sprintf(nom, "%c%d", lettre, annee);
+    }
+    else
+    {
+      // Pas d'enfant cette année
+      sexe = '-';
+      strcpy(nom, "-");
+    }
+
+    // Affiche les résultats
+    printf("%d\t%d\t%d\t%c\t%d\t%s\t%c\n", annee, age, poules, lettre, rot, nom, sexe);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  // Vérifie le nombre de paramètres
+  if (argc != 4)
+  {
+    printf("Usage: %s <nb_poules> <age_depart> <age_fin>\n", argv[0]);
+    return 1;
+  }
+
+  // Récupère les paramètres d'entrée
+  int nb_poules = atoi(argv[1]);
+  int age_depart = atoi(argv[2]);
+  int age_fin = atoi(argv[3]);
+
+  // Vérifie les valeurs des paramètres
+  if (!valider_params(nb_poules, age_depart, age_fin))
+    return 1;
+
+  // Affiche l'entête du tableau
+  afficher_entete();
+
+  // Calcule la population de poules pour chaque année
+  calculer_population(nb_poules, age_depart, age_fin);
+
+  // Tout s'est bien passé
+  return 0;
+}
+
+
